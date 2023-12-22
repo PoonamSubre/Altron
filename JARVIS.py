e197c38ae9bd8617e3ef08ac7484fc58e7d2273c
PoonamSubre <98448868+PoonamSubre@users.noreply.github.com>
2023-12-22 12:46:50 +0530

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "poojasubre@gmail.com"
+                to = "poonamsubre@gmail.com"
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
